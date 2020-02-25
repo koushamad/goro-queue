a2724a36b363b2906b691ad56b61594289e81750
kousha <kousha.ghodsizad@gmail.com>
2020-02-25 17:49:53 +0300

    fix bug

--- config/queue.go
+++ config/queue.go
@@ -1,14 +1,14 @@
 package config
 
 import (
-	"github.com/koushamad/goro-core/app/helper"
+	"github.com/koushamad/goro-app/app/helper"
 	"github.com/streadway/amqp"
 )
 
 var (
 	// rabbit | chanel
-	Queue = map[string]string {
-		"driver": helper.Env("QUEUE_DRIVER", "chanel"),
+	Queue = map[string]string{
+		"driver":  helper.Env("QUEUE_DRIVER", "chanel"),
 		"workers": helper.Env("QUEUE_WORKERS", ""),
 		"traffic": helper.Env("QUEUE_TRAFFIC", ""),
 	}
@@ -22,20 +22,20 @@ var (
 )
 
 type RabbitMQConfig struct {
-	QueueName string `default:"test"`	// queue-name
-	Durable bool `default:"true"`		// durable
-	AutoDelete bool `default:"false"`	// delete when unused
-	Exclusive bool `default:"false"`	// exclusive
-	NoWait bool `default:"false"`		// no-wait
-	Arguments amqp.Table `default:"-"`	// arguments
-	Exchange string `default:""`		// exchange
-	RoutingKey string `default:"test"`	// routing key
-	Mandatory bool `default:"false"`	// mandatory
-	Immediate bool `default:"false"`	// immediate
-	Consumer string `default:""`		// consumer
-	AutoAck bool `default:"false"`		// auto-ack
-	NoLocal bool `default:"false"`		// auto-ack
-	PrefetchCount int `default:"1"`		// prefetch-count
-	PrefetchSize int `default:"0"`		// prefetch-count
-	Global bool `default:"false"`		// global
+	QueueName     string     `default:"test"`  // queue-name
+	Durable       bool       `default:"true"`  // durable
+	AutoDelete    bool       `default:"false"` // delete when unused
+	Exclusive     bool       `default:"false"` // exclusive
+	NoWait        bool       `default:"false"` // no-wait
+	Arguments     amqp.Table `default:"-"`     // arguments
+	Exchange      string     `default:""`      // exchange
+	RoutingKey    string     `default:"test"`  // routing key
+	Mandatory     bool       `default:"false"` // mandatory
+	Immediate     bool       `default:"false"` // immediate
+	Consumer      string     `default:""`      // consumer
+	AutoAck       bool       `default:"false"` // auto-ack
+	NoLocal       bool       `default:"false"` // auto-ack
+	PrefetchCount int        `default:"1"`     // prefetch-count
+	PrefetchSize  int        `default:"0"`     // prefetch-count
+	Global        bool       `default:"false"` // global
 }
